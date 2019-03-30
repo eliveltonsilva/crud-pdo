4776560a4a7dd9aba88820ac0231790c799a9a07
elivelton <websystem.elivelton@gmail.com>
2019-03-30 20:16:42 -0300

    commit final

--- pdo.php
+++ pdo.php
@@ -7,12 +7,36 @@ try{
     /*salvar no banco*/
     $stmt = $pdo->prepare('INSERT INTO clientes (nome, idade) VALUES (:nome, :idade)');
     $stmt->execute(array(
-        ':nome' => 'Fulano',
+        ':nome' => 'Fulano2',
         ':idade' => '26'
     ));
+    
+    echo $stmt->rowCount() . '<br/>';
 
-    $stmt = $pdo->prepare("SELECT * FROM clientes");
-    echo $stmt->rowCount();
+    $id = 1;
+    $nome = "Fulano2";
+    $idade = 12;
+
+    /*update no banco*/
+    $stmt = $pdo->prepare("UPDATE clientes SET nome = :nome, idade = :idade WHERE id = :id");
+    $stmt->execute(array(
+        ':nome' => $nome,
+        ':idade' => $idade,
+        ':id' => $id
+    ));
+
+    /*update no banco*/
+    $stmt = $pdo->prepare("DELETE FROM clientes WHERE id = :id");
+    $stmt->execute(array(
+        ':id' => 1
+    ));
+
+    /*select no banco*/
+    $consult = $pdo->query("SELECT * FROM clientes");
+    while($line = $consult->fetch(PDO::FETCH_ASSOC)){
+        echo "Id: {$line['id']} - Nome: {$line['nome']} - Idade: {$line['idade']}<br/>";
+    }
+
 }catch(PDOException $e){
     echo 'Error: ' . $e->getMessage();
 }
