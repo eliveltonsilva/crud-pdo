532b5f9a682d11b1959b6d69fb93a8b39a901860
elivelton <websystem.elivelton@gmail.com>
2019-03-30 01:11:54 -0300

    pdo salvar

--- pdo.php
+++ pdo.php
@@ -1,3 +1,17 @@
 <?php 
 
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=bd', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->prepare('INSERT INTO clientes (nome, idade) VALUES (:nome, :idade)');
+    $stmt->execute(array(
+        ':nome' => 'Fulano',
+        ':idade' => '26'
+    ));
+    echo $stmt->rowCount();
+}catch(PDOException $e){
+    echo 'Error: ' . $e->getMessage();
+}
+
  ?>
